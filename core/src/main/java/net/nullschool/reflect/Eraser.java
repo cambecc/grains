a0444895723dad67f0ab233656bcfcd518e1df19
Cameron Beccario <cambecc@nullschool.net>
2013-05-02 14:47:50 +0900

    unit tests and javadoc for Eraser

--- core/src/main/java/net/nullschool/reflect/Eraser.java
+++ core/src/main/java/net/nullschool/reflect/Eraser.java
@@ -5,37 +5,70 @@ import java.lang.reflect.*;
 /**
  * 2013-04-03<p/>
  *
+ * A type operator that performs
+ * <a href="http://www.angelikalanger.com/GenericsFAQ/FAQSections/TechnicalDetails.html#What%20is%20type%20erasure?">
+ * type erasure</a>.
+ *
  * @author Cameron Beccario
  */
 final class Eraser extends AbstractTypeOperator<Class<?>> {
 
     static final Eraser INSTANCE = new Eraser();
 
-
+    /**
+     * The erasure of a class object is the class object itself. Object --> Object
+     */
     @Override public Class<?> invoke(Class<?> clazz) {
         return clazz;
     }
 
+    /**
+     * The erasure of a parameterized type is the erasure of its raw type. Set&lt;Long&gt; --> Set
+     *
+     * @throws NullPointerException if the argument is null.
+     */
     @Override public Class<?> invoke(ParameterizedType pt) {
         return invoke(pt.getRawType());
     }
 
+    /**
+     * The erasure of a generic array type is the class object representing an array of the erasure of the
+     * component type. E[] --> Object[]
+     *
+     * @throws NullPointerException if the argument is null.
+     */
     @Override public Class<?> invoke(GenericArrayType gat) {
         return TypeTools.buildArrayType(invoke(gat.getGenericComponentType()));
     }
 
+    /**
+     * The erasure of a wildcard type is Object for unbounded or lower-bounded wildcards, and is the erasure of
+     * the upper bound for upper-bounded wildcards. Examples:
+     * <ul>
+     *     <li>? --> Object.class</li>
+     *     <li>? super Number --> Object.class</li>
+     *     <li>? extends Callable&lt;Number&gt; --> Callable.class</li>
+     * </ul>
+     *
+     * @throws NullPointerException if the argument is null.
+     */
     @Override public Class<?> invoke(WildcardType wt) {
-        if (wt.getLowerBounds().length != 0) {
-            return Object.class;  // UNDONE: research if this is correct
-        }
-        else {
-            return invoke(wt.getUpperBounds()[0]);  // UNDONE: research if this is correct
-                                                    // wouldn't it be the least common type of all bounds?
-        }
+        return wt.getLowerBounds().length != 0 ?
+            Object.class :
+            invoke(wt.getUpperBounds()[0]);
     }
 
+    /**
+     * The erasure of a type variable is its left-most bound or Object if unbounded. Examples:
+     * <ul>
+     *     <li>E --> Object.class</li>
+     *     <li>E extends Enum&lt;E&gt; --> Enum.class</li>
+     *     <li>E extends Serializable & Comparable&lt;E&gt; --> Serializable.class</li>
+     * </ul>
+     *
+     * @throws NullPointerException if the argument is null.
+     */
     @Override public Class<?> invoke(TypeVariable<?> tv) {
-        return invoke(tv.getBounds()[0]);  // UNDONE: research the actual erasure for a multi-bounded type variable
-                                           // wouldn't it be the least common type of all bounds?
+        return invoke(tv.getBounds()[0]);
     }
 }
