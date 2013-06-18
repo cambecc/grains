35350e6729456ce3905f2062da1e74a978abd577
Cameron Beccario <cambecc@nullschool.net>
2013-06-18 16:21:31 +0900

    add javadoc for Grain and GrainBuilder interfaces

--- core/src/main/java/net/nullschool/grains/Grain.java
+++ core/src/main/java/net/nullschool/grains/Grain.java
@@ -25,40 +25,31 @@ import java.util.Map;
 /**
  * 2013-03-24<p/>
  *
- * A Grain is an immutable map of Strings to Objects with the special property that some keys are permanent and
- * restrict the kinds of values they are associated with.<p/>
+ * A Grain is an {@link ConstMap immutable map} of String to Object that contains permanent keys, i.e., keys that
+ * cannot be disassociated from the map. Grains permit {@code null} values but disallow {@code null} keys.<p/>
  *
- * The set of permanent keys and their default values defines the "basis" of the grain. All instances of a
- * particular Grain implementation share the same set of basis keys, and each basis key restricts the values it
- * can be associated with to a specific type or even instance. This is analogous to how all instances of a class
- * share the same set of fields, and each field can store values only of a particular type.<p/>
+ * The set of permanent keys, called the "basis", differs depending on the grain implementation, and each basis key
+ * may restrict the type of values it can be associated with. This is roughly analogous to how instances of a class
+ * share the same set of fields, and each field stores values only of a particular type. Attempts to disassociate a
+ * basis key or associate it with {@code null} results in the key becoming associated with its <i>default value</i>.
+ * Generally, a key's default value is {@code null} if its type restriction is a reference type, otherwise {@code 0}
+ * for primitive types. However, some grain implementations may (optionally) specify a non-null/non-zero default
+ * value for a key.<p/>
  *
- * Grains can also hold keys beyond those defined by their basis. These "extensions" appear as normal associations
- * when iterating over the Grain's keySet, values, or entrySet, but can also be viewed independently via the
- * {@link #extensions} method. Extension keys may also (optionally) restrict their associated values.<p/>
+ * A grain can associate and disassociate non-basis keys, just like a normal map. To differentiate them from the
+ * basis, these keys are called "extensions". Extension keys may also (optionally) restrict their associated
+ * values.<p/>
  *
- * Grain entries can be "modified" using the {@link #with} and {@link #without} methods inherited from
- * {@link ConstMap}. These methods return a new Grain instance, leaving the original instance unmodified. Invoking
- * {@code without} on an <i>extension</i> key will produce a Grain with the extension key removed. However, because
- * <i>basis</i> keys are permanent, invoking {@code without} on a basis key produces a Grain with the key's associated
- * value reset back to its default value.<p/>
+ * The {@link #keySet}, {@link #values}, {@link #entrySet}, and {@link #iterator} methods present the <i>union</i>
+ * of the basis and extensions. There is no guaranteed iteration order. When desired, the {@link #extensions} method
+ * provides a view of just the extension associations.<p/>
  *
- * As it can be expensive to construct Grain instances from a large sequence of {@code with} method invocations, the
- * builder pattern is utilized to simplify Grain construction. A mutable {@link GrainBuilder} instance containing the
- * current associations of a Grain can be retrieved using the {@link #newBuilder} method. After the builder is modified
- * as desired, the {@link GrainBuilder#build} method is invoked to construct a new immutable Grain instance.<p/>
- *
- * A Grain has no defined iteration order. Whether or not {@code null} keys are allowed is implementation
- * dependent. UNDONE: be squishy on nulls?<p/>
- *
- * UNDONE: a discussion on the equivalence of null and a basis key's default value. Null is always taken to mean
- *         the default value of a basis key. Thus, if a basis key has a non-null default value, then the literal
- *         value _null_ can never be associated with the key, as any attempt to make such an association would
- *         reset the value back to the non-null default value.
- *
- * UNDONE: how strong of a requirement to make for extension values being immutable? There is no way to enforce this
- *         at runtime except to check that the type is an instance of any of the allowable basis key types. DEEP
- *         IMMUTABILITY.
+ * Entries may be "modified" using the {@link #with} and {@link #without} methods inherited from {@link ConstMap}.
+ * These methods return a new instance, leaving the original instance unmodified. As it can be expensive to construct
+ * grain instances from a large sequence of {@code with} method invocations, the builder pattern is provided to
+ * simplify construction. A mutable {@link GrainBuilder} instance containing the grain's current associations can be
+ * retrieved using the {@link #newBuilder} method. After the builder is modified as desired, the {@link
+ * GrainBuilder#build} method can be invoked to construct a new grain instance.<p/>
  *
  * @author Cameron Beccario
  */
@@ -66,49 +57,69 @@ public interface Grain extends ConstMap<String, Object> {
 
     /**
      * Returns a grain containing the entries of this grain plus the specified key-value association. No visible
-     * change to this grain occurs. If the specified key is not a basis key, then the key-value association becomes
-     * an extension on this grain.<p/>
+     * change to this grain occurs.<p/>
      *
      * This method may throw ClassCastException if the value does not meet the typing requirements of the key, or
-     * IllegalArgumentException if some property of the value is not suitable for the key. Furthermore, the value
-     * {@code null} is taken to represent the <i>default value</i> of the key.
+     * IllegalArgumentException if some property of the value is not suitable for the key. This method interprets
+     * a {@code null} value to represent the <i>default value</i> of the key. Generally, a key's default value is
+     * {@code null} if its type restriction is a reference type, otherwise {@code 0} for primitive types. However,
+     * some keys may have a non-null/non-zero default value. For example, if a key's default value is <i>42</i>,
+     * then invoking this method with a {@code null} value will associate that key with <i>42</i>.<p/>
+     *
+     * If the specified key is not a basis key, then the key-value association becomes an extension on this grain.
      *
      * @param key the key to associate.
      * @param value the value to be associated with the key.
      * @return a grain containing this grain's associations conjoined with the specified association.
+     * @throws NullPointerException if the key is null.
      * @throws ClassCastException if the value is of a type not suitable for this grain.
-     * @throws NullPointerException if the key is null and this grain does not allow null keys. UNDONE: squishy on nulls?
      * @throws IllegalArgumentException if some property of the value is not suitable for this grain.
      */
     @Override Grain with(String key, Object value);
 
     /**
      * Returns a grain containing the entries of this grain plus all the entries of the specified map. No visible
-     * change to this grain occurs. For any entry in the specified map whose key is not a basis key of this grain,
-     * the entry becomes an extension on this grain.<p/>
+     * change to this grain occurs.<p/>
      *
-     * The effect of this call is equivalent to invoking {@link #with} for each entry in the specified map. Therefore,
-     * this method may throw ClassCastException if any value does not meet the typing requirements or is not suitable
-     * for its associated key. Furthermore, the value {@code null} is taken to represent the <i>default value</i> of
-     * its associated key.
+     * The effect of this call is equivalent to invoking {@link #with} for each entry in the specified map.
      *
      * @param map mappings to conjoin with the associations in this grain.
      * @return a grain containing this grain's associations conjoined with the associations in the provided map.
+     * @throws NullPointerException if the map is null, or any key is null.
      * @throws ClassCastException if any value is of a type not suitable for this grain.
-     * @throws NullPointerException if the map is null, or any key is null and this grain does not allow null keys. UNDONE: squishy on nulls?
      * @throws IllegalArgumentException if some property of any value in the map is not suitable for this grain.
      */
     @Override Grain withAll(Map<? extends String, ?> map);
 
     /**
-     * UNDONE
+     * Returns a grain containing the associations of this grain minus the mapping for the specified key. No visible
+     * change to this grain occurs.<p/>
+     *
+     * NOTE: This method violates the ConstMap contract if invoked with a basis key. Rather than remove the key, this
+     * method associates it with its <i>default value</i>.
+     *
+     * @param key the key to de-associate.
+     * @return a grain containing this grain's associations disjoined from the specified association.
+     * @throws NullPointerException if the key is null.
+     * @throws ClassCastException if the key is not a String.
      */
     @Override Grain without(Object key);
 
     /**
-     * UNDONE
+     * Returns a grain containing the associations of this grain minus all the mappings for the specified keys. No
+     * visible change to this grain occurs.<p/>
+     *
+     * The effect of this call is equivalent to invoking {@link #without} for each key in the specified collection.<p/>
+     *
+     * NOTE: This method violates the ConstMap contract if any key in the specified collection is a basis key. Rather
+     * than remove the key, this method associates it with its <i>default value</i>.
+     *
+     * @param keys the keys to de-associate from this grain.
+     * @return a grain containing this grain's associations disjoined from the keys in the provided collection.
+     * @throws NullPointerException if the specified collection is null, or any key in the collection is null.
+     * @throws ClassCastException if any key in the specified collection is not a String.
      */
-    @Override Grain withoutAll(Collection<?> c);
+    @Override Grain withoutAll(Collection<?> keys);
 
     /**
      * Returns a new builder initialized with the same entries as this grain.
@@ -124,4 +135,14 @@ public interface Grain extends ConstMap<String, Object> {
      * @return a map of all extensions on this grain, or an empty map if there are none. Never returns {@code null}.
      */
     ConstMap<String, Object> extensions();
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean equals(Object obj);
+
+    /**
+     * {@inheritDoc}
+     */
+    int hashCode();
 }
--- core/src/main/java/net/nullschool/grains/GrainBuilder.java
+++ core/src/main/java/net/nullschool/grains/GrainBuilder.java
@@ -18,20 +18,132 @@ package net.nullschool.grains;
 
 import net.nullschool.collect.IterableMap;
 
+import java.util.*;
+
 
 /**
  * 2013-03-24<p/>
  *
+ * A GrainBuilder is a mutable {@link IterableMap iterable map} of String to Object that contains permanent keys,
+ * i.e., keys that cannot be disassociated from the map. GrainBuilders permit {@code null} values but disallow
+ * {@code null} keys. A GrainBuilder is the mutable analog to a {@link Grain} and makes no guarantees for thread
+ * safety.<p/>
  *
+ * The set of permanent keys, called the "basis", differs depending on the builder implementation, and each basis key
+ * may restrict the type of values it can be associated with. This is roughly analogous to how instances of a class
+ * share the same set of fields, and each field stores values only of a particular type. Attempts to disassociate a
+ * basis key or associate it with {@code null} results in the key becoming associated with its <i>default value</i>.
+ * Generally, a key's default value is {@code null} if its type restriction is a reference type, otherwise {@code 0}
+ * for primitive types. However, some builder implementations may (optionally) specify a non-null/non-zero default
+ * value for a key.<p/>
+ *
+ * A builder can associate and disassociate non-basis keys, just like a normal map. To differentiate them from the
+ * basis, these keys are called "extensions". Extension keys may also (optionally) restrict their associated
+ * values.<p/>
+ *
+ * The {@link #keySet}, {@link #values}, {@link #entrySet}, and {@link #iterator} methods present the <i>union</i>
+ * of the basis and extensions. There is no guaranteed iteration order.<p/>
+ *
+ * Builders are intended to simplify the construction of immutable grains. Instances of a builder can be created
+ * by calling the {@link Grain#newBuilder} or {@link GrainFactory#getNewBuilder} methods. The {@link #build} method
+ * constructs a new grain from the builder's current set of entries.<p/>
  *
  * @author Cameron Beccario
  */
 public interface GrainBuilder extends IterableMap<String, Object> {
 
     /**
-     * Constructs a Grain instance from this builder's current state.
+     * {@inheritDoc}<p/>
      *
-     * @return the grain instance.
+     * This method interprets a {@code null} value to represent the <i>default value</i> of the key. Generally, a
+     * key's default value is {@code null} if its type restriction is a reference type, otherwise {@code 0} for
+     * primitive types. However, some keys may have a non-null/non-zero default value. For example, if a key's
+     * default value is <i>42</i>, then invoking this method with a {@code null} value will associate that key with
+     * <i>42</i>.
+     *
+     * @param key the key to associate.
+     * @param value the value to be associated with the key.
+     * @throws NullPointerException if the key is null.
+     * @throws ClassCastException if the value is of a type not suitable for this builder.
+     * @throws IllegalArgumentException if some property of the value is not suitable for this builder.
+     */
+    Object put(String key, Object value);
+
+    /**
+     * {@inheritDoc}<p/>
+     *
+     * @param map mappings to be stored in this builder.
+     * @throws NullPointerException if the map is null, or any key is null.
+     * @throws ClassCastException if any value is of a type not suitable for this builder.
+     * @throws IllegalArgumentException if some property of any value in the map is not suitable for this builder.
+     */
+    void putAll(Map<? extends String, ?> map);
+
+    /**
+     * Removes the mapping for a key if it exists.<p/>
+     *
+     * NOTE: This method violates the Map contract if invoked with a basis key. Rather than remove the key, this
+     * method associates it with its <i>default value</i>.
+     *
+     * @param key the key to de-associate.
+     * @throws NullPointerException if the key is null.
+     * @throws ClassCastException if the key is not a String.
+     */
+    Object remove(Object key);
+
+    /**
+     * Removes all extension mappings from this builder and resets all basis keys to their <i>default values</i>.<p/>
+     *
+     * NOTE: This method violates the Map contract if this builder contains basis keys. Basis keys are permanent,
+     * so the builder will not be empty after this method returns.
+     */
+    void clear();
+
+    /**
+     * {@inheritDoc}<p/>
+     *
+     * NOTE: This method violates the Map contract if basis keys are removed from the resulting set. Rather than
+     * remove the key, the key will become associated with its <i>default value</i>.
+     */
+    Set<String> keySet();
+
+    /**
+     * {@inheritDoc}<p/>
+     *
+     * NOTE: This method violates the Map contract if a value associated with a basis key is removed from the
+     * resulting collection. Rather than remove the association, the key will become associated with its <i>default
+     * value</i>.
+     */
+    Collection<Object> values();
+
+    /**
+     * {@inheritDoc}<p/>
+     *
+     * The entries in the resulting set can be used to assign values in this builder using the {@link Entry#setValue}
+     * method. However, {@code null} is interpreted to represent the <i>default value</i> of the key. See
+     * {@link #put}.<p/>
+     *
+     * NOTE: This method violates the Map contract if an entry for a basis key is removed from the resulting set.
+     * Rather than remove the association, the key will become associated with its <i>default value</i>.
+     */
+    Set<Entry<String, Object>> entrySet();
+
+    /**
+     * Constructs a Grain instance from this builder's current state. This method may be invoked multiple times.
+     *
+     * @return a grain instance.
+     * @throws IllegalArgumentException if some property of this builder is not suitable for the grain being
+     *                                  constructed.
      */
     Grain build();
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean equals(Object obj);
+
+    /**
+     * {@inheritDoc}
+     */
+    int hashCode();
 }
